161850286e62cfa2847bd20f10f70f8537a8644b
Mitch Chanza <hello@mitch.guru>
2023-05-18 12:51:15 +0200

    feat: add function html page

--- functions.js
+++ functions.js
@@ -1,19 +1,35 @@
 const num = [34, 56, 65,23];
+const body = document.querySelector("body");
+const ul1 = document.createElement("ul")
+const ul2 = document.createElement("ul")
 
+body.appendChild(ul1);
+body.appendChild(ul2);
+const createLi = (value,ul,option)=> {
+    const li = document.createElement("li");
+    // li.textContent = option==="double"? `${value} Doubled ${value*2}`: `${value%2 && value}`;
+    if(option ==="double") {
+        li.textContent= `${value} Doubled ${value*2}`
+    } else {
+        if (value%2) {
+            li.textContent = value
+        }
+    }
+    ul.appendChild(li)
+}
 // num.map((value,index)=>db(value,index));
 num.map(db);
 
 
-// // named external fn- this his hoisted
-function db(value,index) {
-    console.log(index,value , 'Doubled =', value*2)
+// // named external fn- tis his hoisted
+function db(value) {
+    createLi(value,ul1, 'double')
+    //console.log(index,value , 'Doubled =', value*2)
 }
 
 // arrow functions are not hoisted. 
 const workFN = (value) =>{
-    if(value%2) {
-        console.log(value)
-    }
+    createLi(value,ul2, 'other')
 }
 num.map(workFN);
 
